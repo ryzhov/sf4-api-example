e901353033668860f50e8656b728653860ea0bf2
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2019-05-02 11:53:07 +0000

    update default controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,17 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Component\Routing\Annotation\Route;
-use Psr\Log\LoggerInterface;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/")
-     */
-    public function index(KernelInterface $kernel, LoggerInterface $logger)
+    public function index(KernelInterface $kernel)
     {
-        $logger->debug('DefaultController::index');
         return $this->json([
             'kernel_version' => Kernel::VERSION,
             'environment' => $kernel->getEnvironment(),
