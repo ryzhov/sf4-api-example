3a20c59a485a9302b266ba13210bb802f7fc41a7
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2019-05-07 12:44:27 +0000

    update user ctrl

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\DTO\UserRegistrationDto;
 use App\Mapper\DTOMapper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,21 +18,18 @@ class UserController extends AbstractController
 {
     /**
      * @Route("/me")
-     *
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function me(UserInterface $user)
+    public function me(UserInterface $user): JsonResponse
     {
         return $this->json(['me' => $user], Response::HTTP_OK, [], ['groups' => 'user']);
     }
 
     /**
      * @Route("/registration", methods={"POST"})
-     *
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
     public function registration(AuthenticationSuccessHandler $authenticationSuccessHandler, DTOMapper $mapper,
-                                 UserPasswordEncoderInterface $encoder, UserRegistrationDto $userRegistrationDto)
+                                 UserPasswordEncoderInterface $encoder, UserRegistrationDto $userRegistrationDto
+    ): JsonResponse
     {
         $mapper->map($userRegistrationDto, $user = new User());
         $user->setPassword($encoder->encodePassword($user, $userRegistrationDto->password));
@@ -45,20 +43,16 @@ class UserController extends AbstractController
 
     /**
      * @Route("/login", methods={"POST"})
-     *
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function login()
+    public function login() : JsonResponse
     {
         return $this->json(['check security configuration firewalls.login.json_login.check_path']);
     }
 
     /**
      * @Route("/logout", methods={"POST"})
-     *
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function logout(UserInterface $user, Request $request)
+    public function logout(UserInterface $user, Request $request) : JsonResponse
     {
         //TODO: put user jwt token in black list until it expired
         $data = ['user' => $user, 'logout_at' => $request->server->get('REQUEST_TIME')];
